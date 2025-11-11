83a00c20faf6334689ae66bc30e7a76f96c03b86
Porramat081 <porramat081@gmail.com>
2025-11-11 23:25:17 +1100

    edit-config-file

--- app/config.ts
+++ app/config.ts
@@ -1,5 +1,5 @@
 export const config = {
-  apiUrl: process.env.NEXT_PUBLIC_APIURL,
-  apiKey: process.env.NEXT_PUBLIC_APIKEY,
-  tokenKey: process.env.NEXT_PUBLIC_TOKENKEY,
+  apiUrl: process.env.NEXT_PUBLIC_APIURL || "",
+  apiKey: process.env.NEXT_PUBLIC_APIKEY || "",
+  tokenKey: process.env.NEXT_PUBLIC_TOKENKEY || "",
 };
